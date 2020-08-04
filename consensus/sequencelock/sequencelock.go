5337be53b6154417254fee77e8fff049f2bb189f
stasatdaglabs <stas@daglabs.com>
2020-08-04 11:04:58 +0300

    [NOD-1190] Move sequenceLock out of the blockdag package.

---
+++ consensus/sequencelock/sequencelock.go
@@ -0,0 +1,151 @@
+package sequencelock
+
+import (
+	"fmt"
+	"github.com/kaspanet/kaspad/consensus/blocknode"
+	"github.com/kaspanet/kaspad/consensus/common"
+	"github.com/kaspanet/kaspad/consensus/pastmediantime"
+	"github.com/kaspanet/kaspad/consensus/utxo"
+	"github.com/kaspanet/kaspad/consensus/virtualblock"
+	"github.com/kaspanet/kaspad/util"
+	"github.com/kaspanet/kaspad/wire"
+)
+
+// SequenceLock represents the converted relative lock-time in seconds, and
+// absolute block-blue-score for a transaction input's relative lock-times.
+// According to SequenceLock, after the referenced input has been confirmed
+// within a block, a transaction spending that input can be included into a
+// block either after 'seconds' (according to past median time), or once the
+// 'BlockBlueScore' has been reached.
+type SequenceLock struct {
+	Milliseconds   int64
+	BlockBlueScore int64
+}
+
+type SequenceLockCalculator struct {
+	virtual               *virtualblock.VirtualBlock
+	pastMedianTimeFactory *pastmediantime.PastMedianTimeFactory
+}
+
+func NewSequenceLockCalculator(virtual *virtualblock.VirtualBlock,
+	pastMedianTimeFactory *pastmediantime.PastMedianTimeFactory) *SequenceLockCalculator {
+
+	return &SequenceLockCalculator{
+		virtual:               virtual,
+		pastMedianTimeFactory: pastMedianTimeFactory,
+	}
+}
+
+// CalcSequenceLock computes a relative lock-time SequenceLock for the passed
+// transaction using the passed UTXOSet to obtain the past median time
+// for blocks in which the referenced inputs of the transactions were included
+// within. The generated SequenceLock lock can be used in conjunction with a
+// block height, and adjusted median block time to determine if all the inputs
+// referenced within a transaction have reached sufficient maturity allowing
+// the candidate transaction to be included in a block.
+func (slc *SequenceLockCalculator) CalcSequenceLock(tx *util.Tx, utxoSet utxo.UTXOSet) (*SequenceLock, error) {
+	return slc.CalcSequenceLockForBlock(slc.virtual.SelectedParent(), utxoSet, tx)
+}
+
+// CalcSequenceLockForBlock computes the relative lock-times for the passed
+// transaction. See the exported version, CalcSequenceLock for further details.
+//
+// This function MUST be called with the DAG state lock held (for writes).
+func (slc *SequenceLockCalculator) CalcSequenceLockForBlock(node *blocknode.BlockNode, utxoSet utxo.UTXOSet, tx *util.Tx) (*SequenceLock, error) {
+	// A value of -1 for each relative lock type represents a relative time
+	// lock value that will allow a transaction to be included in a block
+	// at any given height or time.
+	sequenceLock := &SequenceLock{Milliseconds: -1, BlockBlueScore: -1}
+
+	// Sequence locks don't apply to coinbase transactions Therefore, we
+	// return sequence lock values of -1 indicating that this transaction
+	// can be included within a block at any given height or time.
+	if tx.IsCoinBase() {
+		return sequenceLock, nil
+	}
+
+	mTx := tx.MsgTx()
+	for txInIndex, txIn := range mTx.TxIn {
+		entry, ok := utxoSet.Get(txIn.PreviousOutpoint)
+		if !ok {
+			str := fmt.Sprintf("output %s referenced from "+
+				"transaction %s input %d either does not exist or "+
+				"has already been spent", txIn.PreviousOutpoint,
+				tx.ID(), txInIndex)
+			return sequenceLock, common.NewRuleError(common.ErrMissingTxOut, str)
+		}
+
+		// If the input blue score is set to the mempool blue score, then we
+		// assume the transaction makes it into the next block when
+		// evaluating its sequence blocks.
+		inputBlueScore := entry.BlockBlueScore()
+		if entry.IsUnaccepted() {
+			inputBlueScore = slc.virtual.BlueScore()
+		}
+
+		// Given a sequence number, we apply the relative time lock
+		// mask in order to obtain the time lock delta required before
+		// this input can be spent.
+		sequenceNum := txIn.Sequence
+		relativeLock := int64(sequenceNum & wire.SequenceLockTimeMask)
+
+		switch {
+		// Relative time locks are disabled for this input, so we can
+		// skip any further calculation.
+		case sequenceNum&wire.SequenceLockTimeDisabled == wire.SequenceLockTimeDisabled:
+			continue
+		case sequenceNum&wire.SequenceLockTimeIsSeconds == wire.SequenceLockTimeIsSeconds:
+			// This input requires a relative time lock expressed
+			// in seconds before it can be spent. Therefore, we
+			// need to query for the block prior to the one in
+			// which this input was accepted within so we can
+			// compute the past median time for the block prior to
+			// the one which accepted this referenced output.
+			blockNode := node
+			for blockNode.SelectedParent().BlueScore() > inputBlueScore {
+				blockNode = blockNode.SelectedParent()
+			}
+			medianTime := slc.pastMedianTimeFactory.PastMedianTime(blockNode)
+
+			// Time based relative time-locks have a time granularity of
+			// wire.SequenceLockTimeGranularity, so we shift left by this
+			// amount to convert to the proper relative time-lock. We also
+			// subtract one from the relative lock to maintain the original
+			// lockTime semantics.
+			timeLockMilliseconds := (relativeLock << wire.SequenceLockTimeGranularity) - 1
+			timeLock := medianTime.UnixMilliseconds() + timeLockMilliseconds
+			if timeLock > sequenceLock.Milliseconds {
+				sequenceLock.Milliseconds = timeLock
+			}
+		default:
+			// The relative lock-time for this input is expressed
+			// in blocks so we calculate the relative offset from
+			// the input's blue score as its converted absolute
+			// lock-time. We subtract one from the relative lock in
+			// order to maintain the original lockTime semantics.
+			blockBlueScore := int64(inputBlueScore) + relativeLock - 1
+			if blockBlueScore > sequenceLock.BlockBlueScore {
+				sequenceLock.BlockBlueScore = blockBlueScore
+			}
+		}
+	}
+
+	return sequenceLock, nil
+}
+
+// LockTimeToSequence converts the passed relative locktime to a sequence
+// number.
+func LockTimeToSequence(isMilliseconds bool, locktime uint64) uint64 {
+	// If we're expressing the relative lock time in blocks, then the
+	// corresponding sequence number is simply the desired input age.
+	if !isMilliseconds {
+		return locktime
+	}
+
+	// Set the 22nd bit which indicates the lock time is in milliseconds, then
+	// shift the locktime over by 19 since the time granularity is in
+	// 524288-millisecond intervals (2^19). This results in a max lock-time of
+	// 34,359,214,080 seconds, or 1.1 years.
+	return wire.SequenceLockTimeIsSeconds |
+		locktime>>wire.SequenceLockTimeGranularity
+}
---
+++ consensus/sequencelock/sequencelock_test.go
@@ -0,0 +1,273 @@
+package sequencelock
+
+// TODO: Fix this test
+//// TestCalcSequenceLock tests the LockTimeToSequence function, and the
+//// CalcSequenceLock method of a DAG instance. The tests exercise several
+//// combinations of inputs to the CalcSequenceLock function in order to ensure
+//// the returned SequenceLocks are correct for each test instance.
+//func TestCalcSequenceLock(t *testing.T) {
+//	netParams := &dagconfig.SimnetParams
+//
+//	blockVersion := int32(0x10000000)
+//
+//	// Generate enough synthetic blocks for the rest of the test
+//	dag := newTestDAG(netParams)
+//	node := dag.selectedTip()
+//	blockTime := node.Header().Timestamp
+//	numBlocksToGenerate := 5
+//	for i := 0; i < numBlocksToGenerate; i++ {
+//		blockTime = blockTime.Add(time.Second)
+//		node = newTestNode(dag, blocknode.BlockNodeSetFromSlice(node), blockVersion, 0, blockTime)
+//		dag.blockNodeStore.AddNode(node)
+//		dag.virtual.SetTips(blocknode.BlockNodeSetFromSlice(node))
+//	}
+//
+//	// Create a utxo view with a fake utxo for the inputs used in the
+//	// transactions created below. This utxo is added such that it has an
+//	// age of 4 blocks.
+//	msgTx := wire.NewNativeMsgTx(wire.TxVersion, nil, []*wire.TxOut{{ScriptPubKey: nil, Value: 10}})
+//	targetTx := util.NewTx(msgTx)
+//	utxoSet := utxo.NewFullUTXOSet()
+//	blueScore := uint64(numBlocksToGenerate) - 4
+//	if isAccepted, err := utxoSet.AddTx(targetTx.MsgTx(), blueScore); err != nil {
+//		t.Fatalf("AddTx unexpectedly failed. Error: %s", err)
+//	} else if !isAccepted {
+//		t.Fatalf("AddTx unexpectedly didn't add tx %s", targetTx.ID())
+//	}
+//
+//	// Create a utxo that spends the fake utxo created above for use in the
+//	// transactions created in the tests. It has an age of 4 blocks. Note
+//	// that the sequence lock heights are always calculated from the same
+//	// point of view that they were originally calculated from for a given
+//	// utxo. That is to say, the height prior to it.
+//	outpoint := wire.Outpoint{
+//		TxID:  *targetTx.ID(),
+//		Index: 0,
+//	}
+//	prevUtxoBlueScore := uint64(numBlocksToGenerate) - 4
+//
+//	// Obtain the past median time from the PoV of the input created above.
+//	// The past median time for the input is the past median time from the PoV
+//	// of the block *prior* to the one that included it.
+//	medianTime := dag.PastMedianTime(node.RelativeAncestor(5)).UnixMilliseconds()
+//
+//	// The median time calculated from the PoV of the best block in the
+//	// test DAG. For unconfirmed inputs, this value will be used since
+//	// the MTP will be calculated from the PoV of the yet-to-be-mined
+//	// block.
+//	nextMedianTime := dag.PastMedianTime(node).UnixMilliseconds()
+//	nextBlockBlueScore := int32(numBlocksToGenerate) + 1
+//
+//	// Add an additional transaction which will serve as our unconfirmed
+//	// output.
+//	unConfTx := wire.NewNativeMsgTx(wire.TxVersion, nil, []*wire.TxOut{{ScriptPubKey: nil, Value: 5}})
+//	unConfUtxo := wire.Outpoint{
+//		TxID:  *unConfTx.TxID(),
+//		Index: 0,
+//	}
+//	if isAccepted, err := utxoSet.AddTx(unConfTx, utxo.UnacceptedBlueScore); err != nil {
+//		t.Fatalf("AddTx unexpectedly failed. Error: %s", err)
+//	} else if !isAccepted {
+//		t.Fatalf("AddTx unexpectedly didn't add tx %s", unConfTx.TxID())
+//	}
+//
+//	tests := []struct {
+//		name    string
+//		tx      *wire.MsgTx
+//		utxoSet utxo.UTXOSet
+//		mempool bool
+//		want    *SequenceLock
+//	}{
+//		// A transaction with a single input with max sequence number.
+//		// This sequence number has the high bit set, so sequence locks
+//		// should be disabled.
+//		{
+//			name:    "single input, max sequence number",
+//			tx:      wire.NewNativeMsgTx(1, []*wire.TxIn{{PreviousOutpoint: outpoint, Sequence: wire.MaxTxInSequenceNum}}, nil),
+//			utxoSet: utxoSet,
+//			want: &SequenceLock{
+//				Milliseconds:   -1,
+//				BlockBlueScore: -1,
+//			},
+//		},
+//		// A transaction with a single input whose lock time is
+//		// expressed in seconds. However, the specified lock time is
+//		// below the required floor for time based lock times since
+//		// they have time granularity of 524288 milliseconds. As a result, the
+//		// milliseconds lock-time should be just before the median time of
+//		// the targeted block.
+//		{
+//			name:    "single input, milliseconds lock time below time granularity",
+//			tx:      wire.NewNativeMsgTx(1, []*wire.TxIn{{PreviousOutpoint: outpoint, Sequence: LockTimeToSequence(true, 2)}}, nil),
+//			utxoSet: utxoSet,
+//			want: &SequenceLock{
+//				Milliseconds:   medianTime - 1,
+//				BlockBlueScore: -1,
+//			},
+//		},
+//		// A transaction with a single input whose lock time is
+//		// expressed in seconds. The number of seconds should be 1048575
+//		// milliseconds after the median past time of the DAG.
+//		{
+//			name:    "single input, 1048575 milliseconds after median time",
+//			tx:      wire.NewNativeMsgTx(1, []*wire.TxIn{{PreviousOutpoint: outpoint, Sequence: LockTimeToSequence(true, 1048576)}}, nil),
+//			utxoSet: utxoSet,
+//			want: &SequenceLock{
+//				Milliseconds:   medianTime + 1048575,
+//				BlockBlueScore: -1,
+//			},
+//		},
+//		// A transaction with multiple inputs. The first input has a
+//		// lock time expressed in seconds. The second input has a
+//		// sequence lock in blocks with a value of 4. The last input
+//		// has a sequence number with a value of 5, but has the disable
+//		// bit set. So the first lock should be selected as it's the
+//		// latest lock that isn't disabled.
+//		{
+//			name: "multiple varied inputs",
+//			tx: wire.NewNativeMsgTx(1,
+//				[]*wire.TxIn{{
+//					PreviousOutpoint: outpoint,
+//					Sequence:         LockTimeToSequence(true, 2621440),
+//				}, {
+//					PreviousOutpoint: outpoint,
+//					Sequence:         LockTimeToSequence(false, 4),
+//				}, {
+//					PreviousOutpoint: outpoint,
+//					Sequence: LockTimeToSequence(false, 5) |
+//						wire.SequenceLockTimeDisabled,
+//				}},
+//				nil),
+//			utxoSet: utxoSet,
+//			want: &SequenceLock{
+//				Milliseconds:   medianTime + (5 << wire.SequenceLockTimeGranularity) - 1,
+//				BlockBlueScore: int64(prevUtxoBlueScore) + 3,
+//			},
+//		},
+//		// Transaction with a single input. The input's sequence number
+//		// encodes a relative lock-time in blocks (3 blocks). The
+//		// sequence lock should  have a value of -1 for seconds, but a
+//		// height of 2 meaning it can be included at height 3.
+//		{
+//			name:    "single input, lock-time in blocks",
+//			tx:      wire.NewNativeMsgTx(1, []*wire.TxIn{{PreviousOutpoint: outpoint, Sequence: LockTimeToSequence(false, 3)}}, nil),
+//			utxoSet: utxoSet,
+//			want: &SequenceLock{
+//				Milliseconds:   -1,
+//				BlockBlueScore: int64(prevUtxoBlueScore) + 2,
+//			},
+//		},
+//		// A transaction with two inputs with lock times expressed in
+//		// seconds. The selected sequence lock value for seconds should
+//		// be the time further in the future.
+//		{
+//			name: "two inputs, lock-times in seconds",
+//			tx: wire.NewNativeMsgTx(1, []*wire.TxIn{{
+//				PreviousOutpoint: outpoint,
+//				Sequence:         LockTimeToSequence(true, 5242880),
+//			}, {
+//				PreviousOutpoint: outpoint,
+//				Sequence:         LockTimeToSequence(true, 2621440),
+//			}}, nil),
+//			utxoSet: utxoSet,
+//			want: &SequenceLock{
+//				Milliseconds:   medianTime + (10 << wire.SequenceLockTimeGranularity) - 1,
+//				BlockBlueScore: -1,
+//			},
+//		},
+//		// A transaction with two inputs with lock times expressed in
+//		// blocks. The selected sequence lock value for blocks should
+//		// be the height further in the future, so a height of 10
+//		// indicating it can be included at height 11.
+//		{
+//			name: "two inputs, lock-times in blocks",
+//			tx: wire.NewNativeMsgTx(1,
+//				[]*wire.TxIn{{
+//					PreviousOutpoint: outpoint,
+//					Sequence:         LockTimeToSequence(false, 1),
+//				}, {
+//					PreviousOutpoint: outpoint,
+//					Sequence:         LockTimeToSequence(false, 11),
+//				}},
+//				nil),
+//			utxoSet: utxoSet,
+//			want: &SequenceLock{
+//				Milliseconds:   -1,
+//				BlockBlueScore: int64(prevUtxoBlueScore) + 10,
+//			},
+//		},
+//		// A transaction with multiple inputs. Two inputs are time
+//		// based, and the other two are block based. The lock lying
+//		// further into the future for both inputs should be chosen.
+//		{
+//			name: "four inputs, two lock-times in time, two lock-times in blocks",
+//			tx: wire.NewNativeMsgTx(1,
+//				[]*wire.TxIn{{
+//					PreviousOutpoint: outpoint,
+//					Sequence:         LockTimeToSequence(true, 2621440),
+//				}, {
+//					PreviousOutpoint: outpoint,
+//					Sequence:         LockTimeToSequence(true, 6815744),
+//				}, {
+//					PreviousOutpoint: outpoint,
+//					Sequence:         LockTimeToSequence(false, 3),
+//				}, {
+//					PreviousOutpoint: outpoint,
+//					Sequence:         LockTimeToSequence(false, 9),
+//				}},
+//				nil),
+//			utxoSet: utxoSet,
+//			want: &SequenceLock{
+//				Milliseconds:   medianTime + (13 << wire.SequenceLockTimeGranularity) - 1,
+//				BlockBlueScore: int64(prevUtxoBlueScore) + 8,
+//			},
+//		},
+//		// A transaction with a single unconfirmed input. As the input
+//		// is confirmed, the height of the input should be interpreted
+//		// as the height of the *next* block. So, a 2 block relative
+//		// lock means the sequence lock should be for 1 block after the
+//		// *next* block height, indicating it can be included 2 blocks
+//		// after that.
+//		{
+//			name:    "single input, unconfirmed, lock-time in blocks",
+//			tx:      wire.NewNativeMsgTx(1, []*wire.TxIn{{PreviousOutpoint: unConfUtxo, Sequence: LockTimeToSequence(false, 2)}}, nil),
+//			utxoSet: utxoSet,
+//			mempool: true,
+//			want: &SequenceLock{
+//				Milliseconds:   -1,
+//				BlockBlueScore: int64(nextBlockBlueScore) + 1,
+//			},
+//		},
+//		// A transaction with a single unconfirmed input. The input has
+//		// a time based lock, so the lock time should be based off the
+//		// MTP of the *next* block.
+//		{
+//			name:    "single input, unconfirmed, lock-time in milliseoncds",
+//			tx:      wire.NewNativeMsgTx(1, []*wire.TxIn{{PreviousOutpoint: unConfUtxo, Sequence: LockTimeToSequence(true, 1048576)}}, nil),
+//			utxoSet: utxoSet,
+//			mempool: true,
+//			want: &SequenceLock{
+//				Milliseconds:   nextMedianTime + 1048575,
+//				BlockBlueScore: -1,
+//			},
+//		},
+//	}
+//
+//	t.Logf("Running %v SequenceLock tests", len(tests))
+//	for _, test := range tests {
+//		utilTx := util.NewTx(test.tx)
+//		seqLock, err := dag.CalcSequenceLock(utilTx, utxoSet, test.mempool)
+//		if err != nil {
+//			t.Fatalf("test '%s', unable to calc sequence lock: %v", test.name, err)
+//		}
+//
+//		if seqLock.Milliseconds != test.want.Milliseconds {
+//			t.Fatalf("test '%s' got %v milliseconds want %v milliseconds",
+//				test.name, seqLock.Milliseconds, test.want.Milliseconds)
+//		}
+//		if seqLock.BlockBlueScore != test.want.BlockBlueScore {
+//			t.Fatalf("test '%s' got blue score of %v want blue score of %v ",
+//				test.name, seqLock.BlockBlueScore, test.want.BlockBlueScore)
+//		}
+//	}
+//}
