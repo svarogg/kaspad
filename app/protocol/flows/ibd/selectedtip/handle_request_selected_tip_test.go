f5f4eebc5062b5a75d11983caa21b3e0ab3a7308
yaroslav.r <yaroslav.r@it-dimension.com>
2020-09-02 22:02:10 +0300

    [NOD-1336] Add mid-complexity tests for ibd/selectedtip

--- app/protocol/flows/ibd/selectedtip/handle_request_selected_tip_test.go
+++ app/protocol/flows/ibd/selectedtip/handle_request_selected_tip_test.go
@@ -1,14 +1,15 @@
 package selectedtip
 
 import (
+	"os"
+	"path/filepath"
+	"testing"
+
 	"github.com/kaspanet/kaspad/app/appmessage"
 	"github.com/kaspanet/kaspad/domain/blockdag"
 	"github.com/kaspanet/kaspad/domain/dagconfig"
 	"github.com/kaspanet/kaspad/infrastructure/db/dbaccess"
 	"github.com/kaspanet/kaspad/infrastructure/network/netadapter/router"
-	"os"
-	"path/filepath"
-	"testing"
 )
 
 func TestHandleRequestSelectedTip(t *testing.T) {
@@ -27,8 +28,8 @@ func TestHandleRequestSelectedTip(t *testing.T) {
 
 	dag, err := blockdag.New(&blockdag.Config{
 		DatabaseContext: databaseContext,
-		DAGParams:  &dagconfig.SimnetParams,
-		TimeSource: blockdag.NewTimeSource(),
+		DAGParams:       &dagconfig.SimnetParams,
+		TimeSource:      blockdag.NewTimeSource(),
 	})
 	if err != nil {
 		t.Fatalf("HandleRequestSelectedTip: %s", err)
@@ -37,20 +38,103 @@ func TestHandleRequestSelectedTip(t *testing.T) {
 	ctx := &mockContext{dag: dag}
 	incomingRoute := router.NewRoute()
 	outgoingRoute := router.NewRoute()
-	err = incomingRoute.Enqueue(appmessage.NewMsgRequestSelectedTip())
-	if err != nil {
-		t.Fatalf("HandleRequestSelectedTip: %s", err)
-	}
 
-	go func() {
-		err = HandleRequestSelectedTip(ctx, incomingRoute, outgoingRoute)
+	t.Run("SimpleCall", func(t *testing.T) {
+		go func() {
+			err = HandleRequestSelectedTip(ctx, incomingRoute, outgoingRoute)
+			if err != nil {
+				t.Fatalf("HandleRequestSelectedTip: %s", err)
+			}
+		}()
+
+		err = incomingRoute.Enqueue(appmessage.NewMsgRequestSelectedTip())
 		if err != nil {
 			t.Fatalf("HandleRequestSelectedTip: %s", err)
 		}
-	}()
 
-	_, err = outgoingRoute.DequeueWithTimeout(dequeueTimeout)
-	if err != nil {
-		t.Fatalf("HandleRequestSelectedTip: %s", err)
-	}
+		_, err = outgoingRoute.DequeueWithTimeout(dequeueTimeout)
+		if err != nil {
+			t.Fatalf("HandleRequestSelectedTip: %s", err)
+		}
+	})
+
+	t.Run("CheckOutgoingMessageType", func(t *testing.T) {
+		go func() {
+			err = HandleRequestSelectedTip(ctx, incomingRoute, outgoingRoute)
+			if err != nil {
+				t.Fatalf("HandleRequestSelectedTip: %s", err)
+			}
+		}()
+
+		err = incomingRoute.Enqueue(appmessage.NewMsgRequestSelectedTip())
+		if err != nil {
+			t.Fatalf("HandleRequestSelectedTip: %s", err)
+		}
+
+		msg, err := outgoingRoute.DequeueWithTimeout(dequeueTimeout)
+		if err != nil {
+			t.Fatalf("HandleRequestSelectedTip: %s", err)
+		}
+
+		if _, ok := msg.(*appmessage.MsgSelectedTip); !ok {
+			t.Fatalf("HandleRequestSelectedTip: expected %s, got %s", appmessage.CmdSelectedTip, msg.Command())
+		}
+	})
+
+	t.Run("CallMultipleTimes", func(t *testing.T) {
+		const callTimes = 5
+		go func() {
+			err = HandleRequestSelectedTip(ctx, incomingRoute, outgoingRoute)
+			if err != nil {
+				t.Fatalf("HandleRequestSelectedTip: %s", err)
+			}
+		}()
+
+		for i := 0; i < callTimes; i++ {
+			err = incomingRoute.Enqueue(appmessage.NewMsgRequestSelectedTip())
+			if err != nil {
+				t.Fatalf("HandleRequestSelectedTip: %s", err)
+			}
+
+			_, err := outgoingRoute.DequeueWithTimeout(dequeueTimeout)
+			if err != nil {
+				t.Fatalf("HandleRequestSelectedTip: %s", err)
+			}
+		}
+	})
+	
+	t.Run("CallOnClosedRoute", func(t *testing.T) {
+		closedRoute := router.NewRoute()
+		closedRoute.Close()
+		err = HandleRequestSelectedTip(ctx, closedRoute, outgoingRoute)
+		if err == nil {
+			t.Fatal("HandleRequestSelectedTip: expected error, got nil")
+		}
+	})
+
+	t.Run("CallOnNilRoutes", func(t *testing.T) {
+		err := HandleRequestSelectedTip(ctx, nil, nil)
+		if err == nil {
+			t.Fatal("HandleRejects: expected err, got nil")
+		}
+	})
+
+	t.Run("CallWithEnqueuedInvalidMessage", func(t *testing.T) {
+		go func() {
+			err = HandleRequestSelectedTip(ctx, incomingRoute, outgoingRoute)
+			if err != nil {
+				t.Fatalf("HandleRequestSelectedTip: %s", err)
+			}
+		}()
+
+		err := incomingRoute.Enqueue(appmessage.NewMsgPing(1))
+		if err != nil {
+			t.Fatalf("HandleRejects: %s", err)
+		}
+
+		_, err = outgoingRoute.DequeueWithTimeout(dequeueTimeout)
+		if err != nil {
+			t.Fatalf("HandleRequestSelectedTip: %s", err)
+		}
+	})
 }
--- app/protocol/flows/ibd/selectedtip/request_selected_tip_test.go
+++ app/protocol/flows/ibd/selectedtip/request_selected_tip_test.go
@@ -4,8 +4,10 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/kaspanet/kaspad/app/appmessage"
+
 	peerpkg "github.com/kaspanet/kaspad/app/protocol/peer"
 	"github.com/kaspanet/kaspad/domain/blockdag"
 	"github.com/kaspanet/kaspad/domain/dagconfig"
@@ -40,23 +42,125 @@ func TestRequestSelectedTip(t *testing.T) {
 	ctx := &mockContext{dag: dag}
 	incomingRoute := router.NewRoute()
 	outgoingRoute := router.NewRoute()
-	peer := peerpkg.New(&netadapter.NetConnection{})
 
-	go func() {
-		err = RequestSelectedTip(ctx, incomingRoute, outgoingRoute, peer)
+	t.Run("SimpleCall", func(t *testing.T) {
+		peer := peerpkg.New(&netadapter.NetConnection{})
+
+		go func() {
+			err = RequestSelectedTip(ctx, incomingRoute, outgoingRoute, peer)
+			if err != nil {
+				t.Fatalf("RequestSelectedTip: %s", err)
+			}
+		}()
+
+		peer.RequestSelectedTipIfRequired()
+		_, err := outgoingRoute.DequeueWithTimeout(dequeueTimeout)
 		if err != nil {
 			t.Fatalf("RequestSelectedTip: %s", err)
 		}
-	}()
 
-	peer.RequestSelectedTipIfRequired()
-	_, err = outgoingRoute.Dequeue()
-	if err != nil {
-		t.Fatalf("RequestSelectedTip: %s", err)
-	}
+		err = incomingRoute.Enqueue(appmessage.NewMsgSelectedTip(nil))
+		if err != nil {
+			t.Fatalf("RequestSelectedTip: %s", err)
+		}
+	})
 
-	err = incomingRoute.Enqueue(appmessage.NewMsgSelectedTip(nil))
-	if err != nil {
-		t.Fatalf("RequestSelectedTip: %s", err)
-	}
+	t.Run("CheckOutgoingMessageType", func(t *testing.T) {
+		peer := peerpkg.New(&netadapter.NetConnection{})
+
+		go func() {
+			err = RequestSelectedTip(ctx, incomingRoute, outgoingRoute, peer)
+			if err != nil {
+				t.Fatalf("RequestSelectedTip: %s", err)
+			}
+		}()
+
+		peer.RequestSelectedTipIfRequired()
+		msg, err := outgoingRoute.Dequeue()
+		if err != nil {
+			t.Fatalf("RequestSelectedTip: %s", err)
+		}
+
+		if _, ok := msg.(*appmessage.MsgRequestSelectedTip); !ok {
+			t.Fatalf("RequestSelectedTip: expected %s, got %s", appmessage.CmdRequestSelectedTip, msg.Command())
+		}
+
+		err = incomingRoute.Enqueue(appmessage.NewMsgSelectedTip(nil))
+		if err != nil {
+			t.Fatalf("RequestSelectedTip: %s", err)
+		}
+	})
+
+	t.Run("CallMultipleTimes", func(t *testing.T) {
+		peer := peerpkg.New(&netadapter.NetConnection{})
+
+		const callTimes = 3
+		const minGetSelectedTipInterval = time.Minute
+		go func() {
+			err = RequestSelectedTip(ctx, incomingRoute, outgoingRoute, peer)
+			if err != nil {
+				t.Fatalf("RequestSelectedTip: %s", err)
+			}
+		}()
+
+		for i := 0; i < callTimes; i++ {
+			peer.RequestSelectedTipIfRequired()
+			_, err := outgoingRoute.DequeueWithTimeout(dequeueTimeout)
+			if err != nil {
+				t.Fatalf("RequestSelectedTip: %s", err)
+			}
+
+			err = incomingRoute.Enqueue(appmessage.NewMsgSelectedTip(nil))
+			if err != nil {
+				t.Fatalf("RequestSelectedTip: %s", err)
+			}
+
+			// because of RequestSelectedTipIfRequired internal interval
+			time.Sleep(minGetSelectedTipInterval)
+		}
+	})
+
+	t.Run("CallOnClosedRoute", func(t *testing.T) {
+		peer := peerpkg.New(&netadapter.NetConnection{})
+		closedRoute := router.NewRoute()
+		closedRoute.Close()
+
+		go func() {
+			err = RequestSelectedTip(ctx, incomingRoute, outgoingRoute, peer)
+			if err == nil {
+				t.Fatal("RequestSelectedTip: expected error, got nil")
+			}
+		}()
+
+		peer.RequestSelectedTipIfRequired()
+		_, err := outgoingRoute.DequeueWithTimeout(dequeueTimeout)
+		if err != nil {
+			t.Fatalf("RequestSelectedTip: %s", err)
+		}
+
+	})
+
+	t.Run("CallOnNilRoutes", func(t *testing.T) {
+		peer := peerpkg.New(&netadapter.NetConnection{})
+
+		go func() {
+			err = RequestSelectedTip(ctx, nil, nil, peer)
+			if err == nil {
+				t.Fatal("RequestSelectedTip: expected error, got nil")
+			}
+		}()
+
+		peer.RequestSelectedTipIfRequired()
+		_, err = outgoingRoute.DequeueWithTimeout(dequeueTimeout)
+		if err != nil {
+			t.Fatalf("RequestSelectedTip: %s", err)
+		}
+	})
+
+	t.Run("CallOnNilPeer", func(t *testing.T) {
+		err = RequestSelectedTip(ctx, incomingRoute, outgoingRoute, nil)
+		if err == nil {
+			t.Fatal("RequestSelectedTip: expected error, got nil")
+		}
+	})
 }
